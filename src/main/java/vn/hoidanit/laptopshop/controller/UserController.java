7c096d1e513b14d09a2b93d9bd6ad7605bde2b47
taidungit <dungmount75@gmail.com>
2025-02-15 16:31:31 +0700

    integrate Bootstrap and Jquery

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,10 @@ public class UserController {
     }
     @RequestMapping("/")
     
-    public String getHomePage(){
+    public String getHomePage(Model model){
         String test = this.userService.handleHello();
+        model.addAttribute("mount",test);
+        model.addAttribute("dung","From controller with model");
         return "hello";
     }
 }
