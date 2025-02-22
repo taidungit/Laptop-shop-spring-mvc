4afba3aed8e38c988f5232182a2827ad15c57a20
taidungit <dungmount75@gmail.com>
2025-02-22 15:50:39 +0700

    read write data

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,10 +2,11 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 
@@ -13,18 +14,34 @@ import vn.hoidanit.laptopshop.service.UserService;
 @Controller
 public class UserController {
 
-        private UserService userService;
-    public UserController(UserService userService) {
+        private final UserService userService;
+        private final UserRepository userRepository;
+    public UserController(UserService userService,UserRepository userRepository ) {
         this.userService = userService;
+        this.userRepository=userRepository;
     }
     @RequestMapping("/")
-    
     public String getHomePage(Model model){
         String test = this.userService.handleHello();
         model.addAttribute("mount",test);
         model.addAttribute("dung","From controller with model");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model){
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User());
+   
+        return "admin/user/create";
+    }
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User dungmount){
+       System.out.println("Run here: "+dungmount);
+       this.userRepository.save(dungmount);
+        return "hello";
+    }
+
 }
 
 
